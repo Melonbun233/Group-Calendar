afbffb47fd3da463274d7815a5d2a11b9032b8fd
alicez0626 <alicechang0626@gmail.com>
2018-10-21 22:18:31 -0700

    trying homepage

--- backend/GroupCalendar/routes/index.js
+++ backend/GroupCalendar/routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.send('Group Calendar home page');
-  //res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	//res.send('Group Calendar home page');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
