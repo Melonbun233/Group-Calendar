a9c070e551b8643be1fa368a69c43e6d84defaed
alicez0626 <alicechang0626@gmail.com>
2018-10-22 23:49:26 -0700

    testing user_info_get

--- backend/GroupCalendar/controllers/userController.js
+++ backend/GroupCalendar/controllers/userController.js
@@ -1,7 +1,7 @@
 var User = require('../models/user.js');
 
 exports.user_info_get = function(req, res){
-	User.info_get(function(err, info){
+	User.info_get(req, function(err, info){
 		if (err)
 			res.status(400).send('Invalid id_token');
 		else 
--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -4,8 +4,9 @@ var parser = require('body-parser');
 var user_controller = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', 
-	user_controller.user_info_get(req.body, res));
+router.get('/', function(req, res){
+	user_controller.user_info_get(req.body, res);
+});
 
 router.put('/', user_controller.user_info_put);
 router.post('/', user_controller.user_id_post);
