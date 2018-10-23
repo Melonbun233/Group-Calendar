79ff410e1950d1e704a7dcd035e4c1a3d99a9c27
alicez0626 <alicechang0626@gmail.com>
2018-10-23 00:17:25 -0700

    testing user_info_get

--- backend/GroupCalendar/models/user.js
+++ backend/GroupCalendar/models/user.js
@@ -1,8 +1,8 @@
-var db = require('../db.js');
+//var db = require('../db.js');
 
 
 exports.info_get = function(email, result){
-	db.query("SELECT * FROM users WHERE user_email = ?", [email],
+	db.query('SELECT * FROM users WHERE user_email = ?', [email],
 		function (err, res){
 			console.log('\ndb query working\n');
 			if (err) {
