635d7e32cf9a36779167e33cb4479ca063c9eb3c
alicez0626 <alicechang0626@gmail.com>
2018-10-23 16:35:55 -0700

    testing validator

--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -4,19 +4,20 @@ var parser = require('body-parser');
 var user_controller = require('../controllers/userController');
 
 const {check, validationResult} = require('express-validator/check');
+var temp = {"email" : "234@gmail.com"};
 
 /* GET users. */
 router.get('/', 
 	[check('user_email').isEmail()],
 	function(req, res){
 		//var json = JSON.parse(req);
-		console.log(req.body);
-		const errors = validationResult(req);
+		console.log(temp.email);
+		const errors = validationResult(temp.email);
 		if (!errors.isEmpty()){
 			//console.log(errors);
 			return res.status(400).json({"error": "Invalid user name"});
 		}
-		user_controller.user_info_get(req.body, res);
+		user_controller.user_info_get(temp.email, res);
 	});
 
 router.put('/', user_controller.user_info_put);
