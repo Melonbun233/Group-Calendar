3add8709cb20dc61e5b8d437ab50ea0498b8fc3c
alicez0626 <alicechang0626@gmail.com>
2018-10-22 17:57:06 -0700

    testing GET users, 200

--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -5,8 +5,7 @@ var router = express.Router();
 var user_controller = require('../controllers/userController');
 
 /* GET users listing. */
-//router.get('/users', user_controller.user_info_get);
-router.get('/', (req,res)=>{res.send('***In users router***')});
+router.get('/', user_controller.user_info_get);
 router.put('/', user_controller.user_info_put);
 router.post('/', user_controller.user_id_post);
 router.delete('/', user_controller.user_delete);
