222ccee0386346fee9f9d82de5f2b0698140f33b
alicez0626 <alicechang0626@gmail.com>
2018-10-23 00:23:39 -0700

    testing user_info_get

--- backend/GroupCalendar/db.js
+++ backend/GroupCalendar/db.js
@@ -10,3 +10,15 @@ db.connect(function(err) {
   if (err) throw err;
   console.log('You are now connected to mysql...');
 });
+
+var request = new mysql.Request();
+
+exports.query(query, function(err, res){
+	if (err){
+		console.log("ERROR while query");
+		res.send(err);
+	} else {
+		console.log("Query successful");
+		res.send(res);
+	}
+});
--- backend/GroupCalendar/models/user.js
+++ backend/GroupCalendar/models/user.js
@@ -1,4 +1,4 @@
-//var db = require('../db.js');
+var db = require('../db.js');
 
 
 exports.info_get = function(email, result){
