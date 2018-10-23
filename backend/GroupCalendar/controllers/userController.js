fc5d17490503a13ba37c2cdd875ccc84ce974727
alicez0626 <alicechang0626@gmail.com>
2018-10-23 16:40:31 -0700

    testing validator

--- backend/GroupCalendar/controllers/userController.js
+++ backend/GroupCalendar/controllers/userController.js
@@ -4,7 +4,7 @@ exports.user_info_get = function(req, res){
 	User.info_get(req, function(err, info){
 		if (err) 
 			throw err;
-		else if (info == [])
+		else if (info.length() == 0)
 			res.status(404).json({error: "User name does not refer to any entry."});
 		else
 			res.status(200).json(info);
