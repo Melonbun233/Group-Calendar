d24f5247dfa64dc6473b599fc9ed405dbd51c9dd
alicez0626 <alicechang0626@gmail.com>
2018-10-22 21:02:20 -0700

    testing connection to mysql in models/user.js

--- backend/GroupCalendar/models/user.js
+++ backend/GroupCalendar/models/user.js
@@ -3,11 +3,10 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'secretpw=ZZJ321',
-  database: 'mysql'
+  password: 'secretpw=ZZJ321'
 });
 
 connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected...')
-})
+  if (err) throw err;
+  console.log('You are now connected...');
+});
