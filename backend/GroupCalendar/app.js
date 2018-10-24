7dd3e44d527f4c458a2b9469780ba02fa315b497
alicez0626 <alicechang0626@gmail.com>
2018-10-24 02:54:15 -0700

    edit app.js

--- backend/GroupCalendar/app.js
+++ backend/GroupCalendar/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var UserDB = require('./databases/UserDB');
 var ProjectDB = require('./databases/ProjectDB');
 var CalendarDB = require('./databases/CalendarDB');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 var sqlinjection = require('sql-injection');
 
 /*----require routers-----------*/
@@ -26,19 +26,20 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({type: '*/*'}));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+//app.use(express.json());
+//app.use(express.urlencoded({extended: true}));
 
 
 //app.use(bodyParser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
