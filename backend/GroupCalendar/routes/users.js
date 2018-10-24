6503a598a502fa9ad68dc672e8352b3df82b127b
alicez0626 <alicechang0626@gmail.com>
2018-10-23 23:05:26 -0700

    printing req.body when putting body in url

--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -10,6 +10,7 @@ const {check, validationResult} = require('express-validator/check');
 router.get('/', 
 	check('user_email').isEmail(),
 	function(req, res){
+		console.log(req.body);
 		const errors = validationResult(req);
 		if (!errors.isEmpty()){
 			return res.status(400).json({"error": "Invalid user name."});
