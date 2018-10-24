2788cc96e11cb39609b6a22b8394af391f1b5fb5
Zhuohang Zeng <zeng_zh@foxmail.com>
2018-10-24 05:10:08 -0700

    test

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -7,7 +7,7 @@ var auth_controller = require('../controllers/authGoogleController');
 
 /* GET users listing. */
 router.post('/google', function(req, res){
-	console.log(req.param('id_token'));
+	console.log(req.params.id_token);
 
 	// if(!req.body){
 	// 	res.status(404).send('No request Found\n');
