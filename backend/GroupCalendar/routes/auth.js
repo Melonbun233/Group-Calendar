20f1dd3d50a1c0bf95377b5e5624e1a773e52570
alicez0626 <alicechang0626@gmail.com>
2018-10-24 03:58:51 -0700

    test

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -7,7 +7,7 @@ var auth_controller = require('../controllers/authGoogleController');
 
 /* GET users listing. */
 router.post('/google', function(req, res){
-	//console.log(req);
+	console.log(req);
 
 	if(req.body == {}){
 		res.status(404).send('No request Found\n');
