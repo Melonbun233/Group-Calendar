a2fd32b367cf2d3ba3f9acdeac4398997f646845
alicez0626 <alicechang0626@gmail.com>
2018-10-24 02:40:09 -0700

    moved bodyparser

--- backend/GroupCalendar/app.js
+++ backend/GroupCalendar/app.js
@@ -24,10 +24,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(bodyParser.json({type:'application/json'}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+
+
 //app.use(bodyParser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var parser = require('body-parser');
 var user_controller = require('../controllers/userController');
+const url = require('url');  
+const querystring = require('querystring');
 
 const {check, validationResult} = require('express-validator/check');
 //var temp = {"email" : "234@gmail.com"};
@@ -18,7 +20,7 @@ router.get('/',
 		user_controller.user_info_get(req.param('user_email'), res);
 	});
 
-router.put('/', /*user_controller.user_info_put*/(req,res)=>{
+router.put('/', (req,res)=>{
 	/*
 	if (req.body.has('user_id'))
 		check('user_id').isNumeric();
@@ -27,11 +29,13 @@ router.put('/', /*user_controller.user_info_put*/(req,res)=>{
 	const errors = validationResult(req);
 	if (!errors.isEmpty()){
 		return res.status(400).json({"error": "Invalid info to update."});
-	}*/
-	console.log(req.query.user_name);
+	}*//*
+	console.log(req);
+	var parsedUrl = url.parse(req.originalUrl);  
+let parsedQs = querystring.parse(parsedUrl.query);
 	user_controller.user_info_put(req.query, res);
 	console.log("put works");
-	res.send('put works in send');
+	res.send('put works in send');*/
 
 });
 
