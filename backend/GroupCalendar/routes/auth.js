02a95ad78ad21921d3bd39fbef1845fbed9849f5
Kyle Jiang <kylejoeca@gmail.com>
2018-10-23 18:47:43 -0700

    fix err

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var auth_controller = require('../controllers/authGoogleController');
 
 /* GET users listing. */
-router.post('/auth/google', function(req, res){
+router.post('/google', function(req, res){
 	if(req === null)
 		res.status(404).send('no idtoken found');
 	auth_controller.auth_google(req.body, res);
