3bb35f615eeb79180d7e99090ad686028faf123a
alicez0626 <alicechang0626@gmail.com>
2018-10-24 03:13:45 -0700

    trying querystring

--- backend/GroupCalendar/routes/users.js
+++ backend/GroupCalendar/routes/users.js
@@ -29,13 +29,13 @@ router.put('/', (req,res)=>{
 	const errors = validationResult(req);
 	if (!errors.isEmpty()){
 		return res.status(400).json({"error": "Invalid info to update."});
-	}*//*
+	}*/
 	console.log(req);
 	var parsedUrl = url.parse(req.originalUrl);  
-let parsedQs = querystring.parse(parsedUrl.query);
-	user_controller.user_info_put(req.query, res);
+	var parsedQs = querystring.parse(parsedUrl.query);
+	user_controller.user_info_put(parsedQs, res);
 	console.log("put works");
-	res.send('put works in send');*/
+	res.send('put works in send');
 
 });
 
