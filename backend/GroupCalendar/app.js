fe8751c3ee9a98e0bd1d3beda1b85fde8951c074
Ubuntu <alice@alice.apaukumuscfubgw0hcy4jjp1nb.ux.internal.cloudapp.net>
2018-10-24 09:51:21 +0000

    test

--- backend/GroupCalendar/app.js
+++ backend/GroupCalendar/app.js
@@ -24,8 +24,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(bodyParser.json({type:'application/json'}));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({type: '*/*'}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
