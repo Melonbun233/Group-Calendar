c7f2dffc443ba9b6b3ba3ae57c097443f9726be2
Kyle Jiang <kylejoeca@gmail.com>
2018-10-24 00:02:58 -0700

    test auth.js

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var auth_controller = require('../controllers/authGoogleController');
 
 /* GET users listing. */
-router.post('/google', function(req, res){
+router.post('/', function(req, res){
 
 	console.log('Checking Google Auth');
 
