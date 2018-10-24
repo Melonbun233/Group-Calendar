408d9708e380eff4d7fef5f783e966926e0d37e3
Kyle Jiang <kylejoeca@gmail.com>
2018-10-23 23:29:58 -0700

    test auth.js

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -6,10 +6,12 @@ var auth_controller = require('../controllers/authGoogleController');
 
 /* GET users listing. */
 router.post('/google', function(req, res){
+
+	console.log('Checking Google Auth');
+
 	if(req === null)
 		res.status(404).send('no id_token found');
 
-	console.log('Checking Google Auth');
 	auth_controller.auth_google(req.body, res);
 });
 
