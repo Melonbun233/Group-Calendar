cb1fd0a330c9a86144fb44647b1ba5709a8a054b
Kyle Jiang <kylejoeca@gmail.com>
2018-10-23 18:40:09 -0700

    add auth route

--- backend/GroupCalendar/app.js
+++ backend/GroupCalendar/app.js
@@ -11,6 +11,7 @@ var bodyParser = require('body-parser');
 /*----require routers-----------*/
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 /*------------------------------*/
 
 var app = express();
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth/google',authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
