beabccffbbf8411c1fdb0bc32170d121ea51e3b1
Kyle Jiang <kylejoeca@gmail.com>
2018-10-24 11:14:11 -0700

    update auth.js

--- backend/GroupCalendar/controllers/authGoogleController.js
+++ backend/GroupCalendar/controllers/authGoogleController.js
@@ -75,7 +75,7 @@ exports.auth_google = (req, res) => {
         //   auth_res.status(400).send('Server fails to create a new account.');
         user_id = db_res.user_id;
       });
-      
+
       console.log(user_name);
 
       User.update_user("user_name = '" + user_name + "'", user_id, function(update_err, db_res){
@@ -93,6 +93,11 @@ exports.auth_google = (req, res) => {
     } else {
       // found the exisiting record
       console.log('Welcome Back\n');
+      User.update_user("user_name = '" + user_name + "'", user_res.user_id, function(update_err, db_res){
+        if(update_err)
+          throw update_err;
+      });
+
       res.status(200).json(user_res);
 
     }
