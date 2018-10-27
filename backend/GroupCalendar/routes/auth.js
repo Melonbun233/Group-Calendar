6a3d95fb5bb8c37566258cb2377e2f46ed54ca5c
Kyle Jiang <kylejoeca@gmail.com>
2018-10-27 16:43:23 -0700

    uuid done

--- backend/GroupCalendar/routes/auth.js
+++ backend/GroupCalendar/routes/auth.js
@@ -9,17 +9,11 @@ var auth_controller = require('../controllers/authGoogleController');
 router.post('/google', function(req, res){
 
 	if(req === null){
-		res.status(404).send('No request Found\n');
-		return console('Err: no request\n')
+		res.status(400).send('No request Found');
+		return console('Err: no request')
 	}
 
-	console.log(req.param('id_token'));
-	console.log(req.param('user_email'));
-	console.log(req.param('user_name'));
-	
-	// res.send('auth_post test');
-
- 	auth_controller.auth_google(req, res);
+ 	auth_controller.auth_google(req.body, res);
  	
 });
 
