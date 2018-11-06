9bded4353ae4a48994fccd4b19474a65e8131dd9
Kyle Jiang <kylejoeca@gmail.com>
2018-11-05 22:06:01 -0800

    session done

--- backend/Test/app.js
+++ backend/Test/app.js
@@ -1,22 +1,36 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var session = require('client-sessions');
+
 
 var app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(session({
+	cookieName: 'session',
+	secret: 'secret key',
+	//duration: how long the session will live in milliseconds
+	duration: 200 * 1000,
+	//activeDuration: allows users to lengthen their session by interacting with server
+	activeDuration: 100 * 1000,
+}));
+
 console.log('Entering Test Server...')
 
 app.post('/', function(req, res){
-  console.log('test post');
-  console.log(req.headers);
-  console.log(req.body);
+	console.log('test post');
   // console.log(req);
-  res.send(req.body.user_id);
+  req.session.id = req.body.id;
+  console.log(req.body);
+  console.log(req.session)
+  res.status(200).send(req.session.id);
 });
 
 app.get('/', function(req, res){
   console.log('test get');
-  res.send('success');
+  console.log(req.session);
+  res.status(200).send(req.session.id);
 });
 
-app.listen(8080,'0.0.0.0');
+app.listen(3000,'0.0.0.0');
