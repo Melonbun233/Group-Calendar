49efc602ba0075811a583fdff0ec03dd23fff2ac
Kyle Jiang <kylejoeca@gmail.com>
2018-11-21 15:34:15 -0800

    test fixed

--- backend/GroupCalendar/test/controllers/authController.test.js
+++ backend/GroupCalendar/test/controllers/authController.test.js
@@ -19,202 +19,6 @@ User.updateProfile = jest.fn();
 User.getProfileById = jest.fn();
 User.login = jest.fn();
 
-xdescribe('Testing authGoogle', () => {
-
-	var getInfoSpy = jest.spyOn(AuthController, 'authGoogle');
-
-	describe('Testing by valid req', () => {
-
-		var req = httpMocks.createRequest({
-			session: {
-				uuid: null
-			},
-			body: {
-				idToken: 'abc123',
-				accessToken: '123abc',
-				user: 
-				{ photo: 'https://example.com/photo.jpg',
-				familyName: 'Smith',
-				name: 'Jackal Smith',
-				email: 'jsmith@gmail.com',
-				id: '12345',
-				givenName: 'Jackal' },
-				accessTokenExpirationDate: 3599.8298959732056,
-				serverAuthCode: null,
-				scopes: [] 
-			}
-		});
-
-		describe('Testing without err', () => {
-
-			test('Verified, no userInfo found, return 200', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, false);
-				mockCreateUser(true);
-				mockGetInfo(true, true);
-				mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(200);
-
-			})
-
-			test('Verifed, userInfo found, return 200', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, true);
-				mockUpdateProfile(true);
-				mockUpdateProfile(true);
-				mockGetProfileById(true);
-				
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(200);
-
-			})
-		})
-
-		describe('Testing with err', () => {
-
-			test('Unverifed, return 400', async () => {
-
-				mockVerify(false);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(res.statusCode).toBe(400);
-				expect(getInfoSpy).toHaveBeenCalled();
-
-			})
-
-			test('Verified, no userInfo found, first getInfo err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(false, false);
-				// mockCreateUser(true);
-				// mockGetInfo(true, true);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verified, no userInfo found, createUser err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, false);
-				mockCreateUser(false);
-				// mockGetInfo(true, true);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verified, no userInfo found, second getInfo err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, false);
-				mockCreateUser(true);
-				mockGetInfo(false, false);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verified, no userInfo found twice, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, false);
-				mockCreateUser(true);
-				mockGetInfo(true, false);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verified, no userInfo found, getProfileById err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, false);
-				mockCreateUser(true);
-				mockGetInfo(true, true);
-				mockGetProfileById(false);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-
-			test('Verifed, userInfo found, updateProfile err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, true);
-				mockUpdateProfile(false);
-				// mockUpdateProfile(true);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verifed, userInfo found, second updateProfile err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, true);
-				mockUpdateProfile(true);
-				mockUpdateProfile(false);
-				// mockGetProfileById(true);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-
-			test('Verifed, userInfo found, getProfileById err, return 500', async () => {
-
-				mockVerify(true);
-				mockGetInfo(true, true);
-				mockUpdateProfile(false);
-				mockUpdateProfile(true);
-				mockGetProfileById(false);
-
-				var res = httpMocks.createResponse();
-				await AuthController.authGoogle(req, res);
-				expect(getInfoSpy).toHaveBeenCalled();
-				expect(res.statusCode).toBe(500);
-
-			})
-		})
-	})
-})
-
 describe('Testing authGoogle', () => {
 
 	var getInfoSpy = jest.spyOn(AuthController, 'authGoogle');
@@ -310,6 +114,196 @@ describe('Testing authGoogle', () => {
 		})
 	})
 })
+
+describe('Testing authGoogle', () => {
+
+	var getInfoSpy = jest.spyOn(AuthController, 'authGoogle');
+
+	describe('Testing by valid req', () => {
+
+		var req = httpMocks.createRequest({
+			session: {
+				uuid: null
+			},
+			body: {
+				idToken: 'abc123',
+				accessToken: '123abc',
+				user: 
+				{ photo: 'https://example.com/photo.jpg',
+				familyName: 'Smith',
+				name: 'Jackal Smith',
+				email: 'jsmith@gmail.com',
+				id: '12345',
+				givenName: 'Jackal' },
+				accessTokenExpirationDate: 3599.8298959732056,
+				serverAuthCode: null,
+				scopes: [] 
+			}
+		});
+
+		test('Verified, no userInfo found, return 200', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, false);
+			mockCreateUser(true);
+			mockGetInfo(true, true);
+			mockGetProfileById(true);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(200);
+
+		})
+
+		test('Verifed, userInfo found, return 200', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, true);
+			mockUpdateProfile(true);
+			mockUpdateProfile(true);
+			mockGetProfileById(true);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(200);
+
+		})
+	})
+
+	describe('Testing with err', () => {
+
+		test('Unverifed, return 400', async () => {
+
+			mockVerify(false);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(res.statusCode).toBe(400);
+			expect(getInfoSpy).toHaveBeenCalled();
+
+		})
+
+		test('Verified, no userInfo found, first getInfo err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(false, false);
+
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verified, no userInfo found, createUser err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, false);
+			mockCreateUser(false);
+			
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verified, no userInfo found, second getInfo err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, false);
+			mockCreateUser(true);
+			mockGetInfo(false, false);
+			
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verified, no userInfo found twice, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, false);
+			mockCreateUser(true);
+			mockGetInfo(true, false);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verified, no userInfo found, getProfileById err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, false);
+			mockCreateUser(true);
+			mockGetInfo(true, true);
+			mockGetProfileById(false);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+
+		test('Verifed, userInfo found, updateProfile err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, true);
+			mockUpdateProfile(false);
+			// mockUpdateProfile(true);
+			// mockGetProfileById(true);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verifed, userInfo found, second updateProfile err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, true);
+			mockUpdateProfile(true);
+			mockUpdateProfile(false);
+			// mockGetProfileById(true);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+
+		test('Verifed, userInfo found, getProfileById err, return 500', async () => {
+
+			mockVerify(true);
+			mockGetInfo(true, true);
+			mockUpdateProfile(false);
+			mockUpdateProfile(true);
+			mockGetProfileById(false);
+
+			var res = httpMocks.createResponse();
+			await AuthController.authGoogle(req, res);
+			expect(getInfoSpy).toHaveBeenCalled();
+			expect(res.statusCode).toBe(500);
+
+		})
+	})
+})
+
 
 /* ---------- authApp ----------- */
 describe('Testing authApp', () => {
