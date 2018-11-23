e54793cccb23ad4836e5e867f2ea3b6b01e52f99
Kyle Jiang <kylejoeca@gmail.com>
2018-11-22 17:07:34 -0800

    auth test fixed

--- backend/GroupCalendar/test/controllers/authController.test.js
+++ backend/GroupCalendar/test/controllers/authController.test.js
@@ -162,8 +162,8 @@ describe('Testing authGoogle', () => {
 				await AuthController.authGoogle(req, res);
 				expect(getInfoSpy).toHaveBeenCalled();
 				expect(res.statusCode).toBe(200);
-				console.log(res);
-				expect(res.pwdSet).toBe(true);
+				console.log(res.body);
+				expect(res..body.pwdSet).toBe(true);
 
 			})
 
