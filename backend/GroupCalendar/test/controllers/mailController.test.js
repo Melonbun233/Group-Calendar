5f5a74863f85b2549f15f8dbcb71c4668fbb3d76
Kyle Jiang <kylejoeca@gmail.com>
2018-11-28 02:55:51 -0800

    mail ctrl test

--- backend/GroupCalendar/test/controllers/mailController.test.js
+++ backend/GroupCalendar/test/controllers/mailController.test.js
@@ -6,23 +6,23 @@ const MailController = require('../../controllers/mailController');
  *	NodeMailerMocks
  */
 // jest.mock('nodemailer');
-const transportErr = {
-	sendMail: (data, callback) => {
-		const err = new Error('some error');
-		callback(err, null);
-	},
-	close: () => {
-		return 0;
-	}
-}
-const transport = {
-	sendMail: (data, callback) => {
-		callback(null, null);
-	},
-	close: () => {
-		return 0;
-	}
-}
+// const transportErr = {
+// 	sendMail: (data, callback) => {
+// 		const err = new Error('some error');
+// 		callback(err, null);
+// 	},
+// 	close: () => {
+// 		return 0;
+// 	}
+// }
+// const transport = {
+// 	sendMail: (data, callback) => {
+// 		callback(null, null);
+// 	},
+// 	close: () => {
+// 		return 0;
+// 	}
+// }
 
 
 /**
@@ -36,13 +36,13 @@ const transport = {
  	var getInfoSpy = jest.spyOn(MailController, 'sendEmail');
 
  	test('Testing by without err', () => {
- 		beforeEach(() =>{ 
- 			spyOn(Mailer, 'createTransport').and.callFake(() => {
- 				return transport;
- 			});
- 			// Mailer.createTransport = jest.fn().mockReturnValueOnce(transport);
- 		})
- 		var receiver = "yueruc@gmail.com";
+ 		// beforeEach(() =>{ 
+ 		// 	spyOn(Mailer, 'createTransport').and.callFake(() => {
+ 		// 		return transport;
+ 		// 	});
+ 		// 	// Mailer.createTransport = jest.fn().mockReturnValueOnce(transport);
+ 		// })
+ 		var receiver = "test@mail.com";
  		var subject = "Test";
  		var text = "Successful Test";
  		MailController.sendEmail(receiver, subject, text, text);
@@ -52,11 +52,11 @@ const transport = {
  	
  	test('Testing by with err', () => {
  		
- 		beforeEach(() =>{ 
- 			spyOn(Mailer, 'createTransport').and.callFake(() => {
- 				return transportErr;
- 			});
- 		})
+ 		// beforeEach(() =>{ 
+ 		// 	spyOn(Mailer, 'createTransport').and.callFake(() => {
+ 		// 		return transportErr;
+ 		// 	});
+ 		// })
 
  		var receiver = "123";
  		var subject = "Test";
