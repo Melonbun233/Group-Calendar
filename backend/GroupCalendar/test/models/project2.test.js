8384f4a83eb26cc7bfb39aa3f7f67d063847401b
Kyle Jiang <kylejoeca@gmail.com>
2018-11-28 00:08:06 -0800

    project model test

--- backend/GroupCalendar/test/models/project2.test.js
+++ backend/GroupCalendar/test/models/project2.test.js
@@ -1,10 +1,11 @@
-const Project = require('../../models/project');
+const Project_ = require('../../models/project');
 const ProjectDB = require('../../databases/ProjectDB');
 const CalendarDB = require('../../databases/CalendarDB');
 
 jest.mock('../../databases/UserDB');
 jest.mock('../../databases/ProjectDB');
 jest.mock('../../databases/CalendarDB');
+const Project = new Project_();
 
 /**
  * Mock List:
